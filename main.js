e8029b9bf15575a34d3641f8468390840b75c42e
Andarov <bekturandarob678@gmail.com>
2022-07-16 04:44:40 +0500

    Change global styles and add fonts

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 72.Karra hosil qilish
+// let counter = 7
+// for (let i = 1; i <= 10; i++) {
+//     let result = (counter*i)
+//     console.log(result);
+// }
