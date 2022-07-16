354f41cbeab9de70aa83d6b4a2157bb56d18a9a4
Andarov <bekturandarob678@gmail.com>
2022-07-16 16:32:39 +0500

    Add input value == number boolean

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ elForm.addEventListener ("submit", function(evt) {
     evt.preventDefault();
     const number = elInput.value;
     elContentResult.innerHTML = " ";
-    
-    for (let i = 1; i <= 10; i++) {
-        const result = (number * i);
-        elContentResult.innerHTML += `
-        <div class="wrapper">
-        <span>${number}</span>
-        <span>*</span>
-        <span>${i}</span>
-        <span>=</span>
-        <span>${result}</span>
-        </div>
-        `;
+    if (number) {  
+        for (let i = 1; i <= 10; i++) {
+            const result = (number * i);
+            elContentResult.innerHTML += `
+            <div class="wrapper">
+            <span>${number}</span>
+            <span>*</span>
+            <span>${i}</span>
+            <span>=</span>
+            <span>${result}</span>
+            </div>
+            `;
+        }
     }
 });
